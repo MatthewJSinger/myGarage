873155b8b567eb0f43ff538bfc91e20df9ccbe09
Matthew Singer <73543914+MatthewJSinger@users.noreply.github.com>
2023-08-13 02:15:28 +0100

    test script readability

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from datetime import date
 
 if __name__ == '__main__':
     vehicle = Vehicle(
-        "Motorcycle",
-        "Kawasaki",
-        "ER-6F",
-        date(2016,7,1),
-        "NB16 LYU",
-        2
+        vehicleType="Motorcycle",
+        make="Kawasaki",
+        model="ER-6F",
+        dateReg=date(2016,7,1),
+        reg="NB16 LYU",
+        cylinders=2
         )
     vehicle.setMileage(3000)
     vehicle.setLastMot(2023,7,22)
